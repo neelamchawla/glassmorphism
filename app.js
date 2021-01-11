208ec469aa676254f8d0e683d8edfc6abc919313
neelamchawla <50483784+neelamchawla@users.noreply.github.com>
2021-01-11 14:08:05 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+// Menu
 function menuToggle() {
     const toggleMenu = document.querySelector('.toggle');
     const section = document.querySelector('section');
     toggleMenu.classList.toggle('active');
     section.classList.toggle('active');
-}
+};
 
+// parallax
 let bg = document.getElementById("bgMoon");
 let moon = document.getElementById("moon");
 let mountain = document.getElementById("mountain");
@@ -19,4 +21,42 @@ window.addEventListener('scroll', function() {
     mountain.style.top = -value * 0.15 + 'px';
     road.style.top = value * 0.05 + 'px';
     text.style.top = value * 0.1 + 'px';
-})
+});
+
+// countdown
+const countDate = new Date('march 31, 2021 00:00:00').getTime();
+
+function newYear() {
+    const now = new Date().getTime();
+    gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const d = Math.floor(gap / (day));
+    const h = Math.floor(gap % (day) / (hour));
+    const m = Math.floor(gap % (hour) / (minute));
+    const s = Math.floor(gap % (minute) / (second));
+
+    document.getElementById('day').innerHTML = d;
+    document.getElementById('hour').innerHTML = h;
+    document.getElementById('minute').innerHTML = m;
+    document.getElementById('second').innerHTML = s;
+};
+
+setInterval(function () {
+    newYear();
+});
+
+// product
+function changeColor(event) {
+    // reset and remove active class first
+    document.querySelectorAll('span.active').forEach(function (item) {
+        item.classList.remove('active')
+    })
+
+    // add active class to selected menu item
+    event.target.classList.add('active')
+}
